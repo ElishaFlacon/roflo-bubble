563b162961697631ca81b92cd57dd3ea66c61a2e
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2024-04-06 21:06:17 +0500

    mini update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import React, { CSSProperties, useState } from "react";
+import React, { CSSProperties, useRef, useState } from "react";
 import Draggable from "react-draggable";
+import { Bubble } from "./Bubble";
 
 export const App: React.FC = () => {
-    const [bubbles, setBubbles] = useState<{ index: number; style: CSSProperties }[]>([]);
+    const [currentType, setCurrentType] = useState("native");
+    const [bubbles, setBubbles] = useState<
+        { index: number; style: CSSProperties; currentType: string }[]
+    >([]);
+
+    const containerRef = useRef<HTMLDivElement>(null);
 
     const addNewBubble = () => {
         const size = randomNumber(5, 20);
@@ -14,7 +20,7 @@ export const App: React.FC = () => {
             background: color
         };
 
-        setBubbles((self) => [...self, { index: self.length, style }]);
+        setBubbles((self) => [...self, { index: self.length, style, currentType }]);
     };
 
     const randomNumber = (min: number, max: number) => {
@@ -28,20 +34,54 @@ export const App: React.FC = () => {
 
     return (
         <div className='app'>
+            <div>
+                {"Привет, не забудьте посмотреть мое резюме --> "}
+                <a
+                    className='link'
+                    href='https://eelisey.ru/portfolio-site/sokolov_elisey_frontend_developer.pdf'
+                    target='_blank'
+                >
+                    Жмяв чтобы резюме!
+                </a>
+            </div>
+
+            <div>Сделал 2 вариации, нативная и спомощью либы</div>
+
+            <div> Сейчас выбран тип: {currentType}</div>
+
+            <button
+                className='button-small'
+                onClick={() => {
+                    if (currentType === "react draggable") {
+                        setCurrentType("native");
+                        return;
+                    }
+
+                    setCurrentType("react draggable");
+                }}
+            >
+                Сменить тип шариков
+            </button>
+
             <button className='button' onClick={addNewBubble}>
                 ЖМЯВ ЧТОБЫ ШАРИК
             </button>
 
-            <div className='container'>
-                {bubbles.map((value) => {
-                    return (
+            <div ref={containerRef} className='container'>
+                {bubbles.map((value) =>
+                    value.currentType === "native" ? (
+                        <Bubble containerRef={containerRef} style={value.style}>
+                            {value.index} Нативный
+                        </Bubble>
+                    ) : (
                         <Draggable key={value.index}>
                             <div className='draggable' style={value.style}>
-                                {value.index}
+                                <div>{value.index}</div>
+                                <div>React Draggable</div>
                             </div>
                         </Draggable>
-                    );
-                })}
+                    )
+                )}
             </div>
         </div>
     );
